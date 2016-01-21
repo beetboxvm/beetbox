ab6e69bc16121f3bf6b42add9f0de3dff2b1ce3b
Thom Toogood <thomtoogood@gmail.com>
2016-01-22 11:43:56 +1200

    Added beet_base var to allow overriding the default mapped dir location.

--- tests/dependencies.sh
+++ tests/dependencies.sh
@@ -5,9 +5,9 @@ sudo apt-get update
 sudo apt-get purge apache2 php5-cli mysql-common mysql-server
 sudo rm -rf /etc/apache2/mods-enabled/*
 
-if [ ! -d "$BEETBOX_HOME" ]; then
+if [ ! -d "$BEET_HOME" ]; then
   sudo apt-get install git
-  git clone https://github.com/drupalmel/beetbox.git $BEETBOX_HOME
+  git clone https://github.com/drupalmel/beetbox.git $BEET_HOME
 fi
 
-sudo $BEETBOX_HOME/ansible/build.sh
+sudo $BEET_HOME/ansible/build.sh
