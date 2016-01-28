228223bdb886b571aea30e26db9b167af954a169
Thom Toogood <thomtoogood@gmail.com>
2016-01-28 16:30:07 +1100

    Added packer build.

---
+++ packer/scripts/virtualbox.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install dependancies.
+apt-get -y install build-essential linux-headers-$(uname -r) nfs-common
+
+# Install VirtualBox Guest Additions.
+mkdir -p /mnt/virtualbox
+mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
+sh /mnt/virtualbox/VBoxLinuxAdditions.run
+ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
+umount /mnt/virtualbox
+rm -rf /home/vagrant/VBoxGuest*.iso
