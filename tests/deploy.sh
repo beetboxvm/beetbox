3105de14f7a81616efb7c9ee09d466baaeb4c01b
Thom Toogood <thom.toogood@eurekareport.com.au>
2016-02-02 14:54:59 +1100

    Build all boxes on release.

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -6,5 +6,5 @@ unzip packer_0.8.6_linux_amd64.zip -d ~/packer
 
 # Trigger new build.
 cd ~/$CIRCLE_PROJECT_REPONAME/packer
-sed -i "s/BEET_VERSION/$BEET_VERSION/" template.json
-~/packer/packer push -token=$ATLAS_TOKEN -name="DrupalMel/$BEET_BOX" template.json
+sed -i "s/BEET_VERSION/$BEET_VERSION/" $BEET_TEMPLATE
+~/packer/packer push -token=$ATLAS_TOKEN -name="DrupalMel/$BEET_BOX" $BEET_TEMPLATE
