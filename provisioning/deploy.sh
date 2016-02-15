289be7a987982626844d0d1d54194234a7df5af5
Thom Toogood <thom.toogood@eurekareport.com.au>
2016-02-15 14:14:35 +1100

    Fix deploy.sh use current branch/tag for atlas build.

--- provisioning/deploy.sh
+++ provisioning/deploy.sh
@@ -1,19 +1,24 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 
 # Set default environment variables.
 ATLAS_BOX="$BEET_BOX-dev"
-BEET_VERSION=${BEET_VERSION:-"0.1.$CIRCLE_BUILD_NUM"}
+ATLAS_VERSION=${BEET_VERSION:-"0.1.$CIRCLE_BUILD_NUM"}
+BEET_VERSION=${CIRCLE_BRANCH:-"dev"}
 BEET_TEMPLATE=${BEET_TEMPLATE:-"template.json"}
-PACKER_HOME=${PACKER_HOME:-"~/$CIRCLE_PROJECT_REPONAME/provisioning"}
+PACKER_HOME=${PACKER_HOME:-"$BEET_HOME/provisioning"}
 
 # Use prod atlas build and tag for version.
-if [ $CIRCLE_TAG ]; then
+if [ ! $CIRCLE_TAG = "" ]; then
   ATLAS_BOX="$BEET_BOX"
+  ATLAS_VERSION="$CIRCLE_TAG"
   BEET_VERSION="$CIRCLE_TAG"
 fi
 
+cd $PACKER_HOME
+
 # Update template variables.
+sed -i "s/ATLAS_VERSION/$ATLAS_VERSION/" $BEET_TEMPLATE
 sed -i "s/BEET_VERSION/$BEET_VERSION/" $BEET_TEMPLATE
 
 # Trigger new build.
-cd $PACKER_HOME ; packer push -token=$ATLAS_TOKEN -name="$ATLAS_BOX" $BEET_TEMPLATE
+packer push -token=$ATLAS_TOKEN -name="$ATLAS_BOX" $BEET_TEMPLATE
